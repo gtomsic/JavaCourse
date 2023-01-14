e57491823abda5dc640351548d812916f19b09cf
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-14 16:16:59 -0700

    Using for loop to calculate loan interest

--- ForLoop/src/Main.java
+++ ForLoop/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double moneyToBorrow = 100.0;
+       for(double rate = 7.5; rate <= 10.0; rate = rate + 0.25) {
+           System.out.println(moneyToBorrow + " at " + rate + "% interest = " + calculateInterest(moneyToBorrow, rate));
+       }
+
+    }
+
+    public  static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
     }
 }
