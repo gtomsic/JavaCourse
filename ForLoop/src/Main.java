942b9a6ecf725758b58951e40b79e9ac59288947
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-14 16:23:35 -0700

    Adding break state to for loop

--- ForLoop/src/Main.java
+++ ForLoop/src/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         double moneyToBorrow = 100.0;
        for(double rate = 7.5; rate <= 10.0; rate = rate + 0.25) {
-           System.out.println(moneyToBorrow + " at " + rate + "% interest = " + calculateInterest(moneyToBorrow, rate));
+           double interestAmount = calculateInterest(moneyToBorrow, rate);
+                   if(interestAmount > 8.5) {
+                       break;
+                   };
+           System.out.println(moneyToBorrow + " at " + rate + "% interest = " + interestAmount);
        }
-
     }
 
     public  static double calculateInterest(double amount, double interestRate) {
