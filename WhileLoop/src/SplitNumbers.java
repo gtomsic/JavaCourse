1ec55fbded5cc2c77f803ce69c06f291dd2c1b68
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-15 08:11:21 -0700

    Sum digits challenge complete

---
+++ WhileLoop/src/SplitNumbers.java
@@ -0,0 +1,27 @@
+public class SplitNumbers {
+    public static void main(String[] args){
+        int number = 1234;
+        System.out.println("Total sum from " + number + " = " + sumDigits(number));
+        number = -123;
+        System.out.println("Total sum from " + number + " = " + sumDigits(number));
+        number = 237;
+        System.out.println("Total sum from " + number + " = " + sumDigits(number));
+        number = 4;
+        System.out.println("Total sum from " + number + " = " + sumDigits(number));
+
+    }
+
+    public static int sumDigits(int num) {
+        if(num < 0) {
+            return -1;
+        }
+        int sum = 0;
+
+        while (num > 9) {
+            sum += (num % 10);
+            num = num / 10;
+        }
+        sum += num;
+        return sum;
+    }
+}
