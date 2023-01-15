3b576e7807eecef9bf0f3a877f470a7eee885240
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-14 21:52:41 -0700

    Adding while continue state in while challenge

--- WhileLoop/src/WhileChallenge.java
+++ WhileLoop/src/WhileChallenge.java
@@ -1,11 +1,17 @@
 public class WhileChallenge {
     public static void main(String[] args) {
         int i = 5;
+        int sum = 0;
         while (i <= 20) {
-            boolean isEven = isEvenNumber(i);
-            System.out.println(i + " is " + (isEven ? "Even" : "Odd"));
             i++;
+            boolean isEven = isEvenNumber(i);
+            if(!isEven) {
+                continue;
+            }
+            sum = sum + i;
+            System.out.println(i + " is " + (isEven ? "Even" : "Odd"));
         }
+        System.out.println("Total Sum = " + sum);
     }
 
     public static boolean isEvenNumber(int num) {
