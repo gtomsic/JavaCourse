e071f43b692d59536fdc2e3de9f3c8ed2b3eb00f
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-14 21:04:54 -0700

    Adding while with continue condition

--- WhileLoop/src/DoWhile.java
+++ WhileLoop/src/DoWhile.java
@@ -1,14 +1,28 @@
 public class DoWhile {
     public static void main(String[] args){
-        int j = 1;
-        boolean isReady = false;
+//        int j = 1;
+//        boolean isReady = false;
+//
+//        do{
+//            if(j > 5) {
+//                break;
+//            }
+//            System.out.println(j);
+//            j++;
+//
+//            isReady = (j > 0);
+//        } while (isReady);
 
-        do{
-            if(j > 5) {
-                break;
+        int number = 0;
+        while(number < 50) {
+            number += 5;
+
+            if(number % 25 == 0) {
+                continue;
             }
-            System.out.println(j);
-            j++;
-        } while (isReady);
+
+            System.out.print(number + "_");
+
+        }
     }
 }
