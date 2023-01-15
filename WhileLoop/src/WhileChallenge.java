2907524ee35974fc3590422bd41b0c728ca13242
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-14 22:00:41 -0700

    First while loop challenge complete

--- WhileLoop/src/WhileChallenge.java
+++ WhileLoop/src/WhileChallenge.java
@@ -1,17 +1,23 @@
 public class WhileChallenge {
     public static void main(String[] args) {
-        int i = 5;
-        int sum = 0;
+        int i = 4;
+        int evenCount = 0;
+        int oddCount = 0;
         while (i <= 20) {
             i++;
             boolean isEven = isEvenNumber(i);
             if(!isEven) {
+                oddCount++;
                 continue;
             }
-            sum = sum + i;
             System.out.println(i + " is " + (isEven ? "Even" : "Odd"));
+            evenCount++;
+            if(evenCount >= 5) {
+                break;
+            }
         }
-        System.out.println("Total Sum = " + sum);
+        System.out.println("Total odd numbers found = " + oddCount);
+        System.out.println("Total even numbers found = " + evenCount);
     }
 
     public static boolean isEvenNumber(int num) {
