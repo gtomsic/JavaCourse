e7b59323adbd7f0283c7c9624599995d20edddb4
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-14 20:50:40 -0700

    While statement sample

--- WhileLoop/src/Main.java
+++ WhileLoop/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int j = 1;
+
+        while(j <= 5) {
+            System.out.println(j);
+            j++;
+        }
     }
 }
