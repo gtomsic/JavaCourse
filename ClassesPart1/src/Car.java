7437f8380cbf9783bdc1c8e52378f3589ee39eb2
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-16 20:47:35 -0700

    Create a classes with private attributes setters and getters method

---
+++ ClassesPart1/src/Car.java
@@ -0,0 +1,62 @@
+public class Car {
+    private String make = "Mercedes";
+    private String model = "GLE";
+    private String color = "White";
+    private int doors = 5;
+    private boolean convertible = false;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public  void setMake(String make) {
+        if(make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake) {
+            case "holden", "porsche", "tesla", "mercedes" -> this.make = make;
+            default -> {
+                this.make = "Unsupported";
+            }
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void describeCar() {
+        System.out.println(doors + "-Doors " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
+}
--- ClassesPart1/src/Main.java
+++ ClassesPart1/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car();
+        car.setMake("Mercedes");
+        car.setModel("GLS");
+        car.setColor("Gray");
+        car.setConvertible(false);
+        car.setDoors(5);
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
+
+        Car gabriel = new Car();
+        gabriel.setMake("tesla");
+        gabriel.setModel("X5");
+        gabriel.setColor("GREEN");
+        gabriel.setConvertible(false);
+        gabriel.setDoors(5);
+        System.out.println("make = " + gabriel.getMake());
+        System.out.println("model = " + gabriel.getModel());
+        gabriel.describeCar();
     }
 }
