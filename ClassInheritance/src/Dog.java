404a226e386af1e04fdebc6125647a74dfd67fdb
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-18 21:44:33 -0700

    Adding fish class to extends animal inheritance example

--- ClassInheritance/src/Dog.java
+++ ClassInheritance/src/Dog.java
@@ -23,4 +23,43 @@ public class Dog extends Animal {
                 ", tailShape='" + tailShape + '\'' +
                 "} " + super.toString();
     }
+
+    public void makeNoise() {
+        // Overriding the parent method called makeNoise
+        if(type == "Wolf"){
+            System.out.println("Ow Wooooo! ");
+        }
+        bark();
+        System.out.println();
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+//        System.out.println("Dogs, walk, run and wag their tail.");
+        if( speed == "slow") {
+            walk();
+            wagTail();
+        } else {
+            run();
+            bark();
+        }
+        System.out.println();
+    }
+
+    private void bark() {
+        System.out.println("Woof! ");
+    }
+
+    private void run() {
+        System.out.println("Dog Running! ");
+    }
+
+    private void walk() {
+        System.out.println("Dog Walking! ");
+    }
+
+    private void wagTail() {
+        System.out.println("Dog Wagging Tail! ");
+    }
 }
