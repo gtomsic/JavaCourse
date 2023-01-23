36dc64ee87f5c6efce94406ad6588d95f955c869
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-22 21:37:35 -0700

    Adding Scanner in Polymorphism complete

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       Movie theMovie = new Adventure("Star Wars");
-       theMovie.watchMovie();
+//       Movie theMovie = Movie.getMovie("Romance", "Star Wars");
+//       theMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.println("Enter (A for adventure, C for Comedy, " +
+                    "S for Science Fiction, or Q to Quit");
+            String type = s.nextLine();
+            if("Qq".contains(type)) {
+                break;
+            }
+            System.out.print("Enter Movie Title: ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
     }
 }
