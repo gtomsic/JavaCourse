41dbc03fa0dd26b96aa287640164af2cee11a358
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-24 16:43:10 -0700

    Intro to array topic completed

--- ArrayProject/src/Main.java
+++ ArrayProject/src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      int[] myIntArray = new int[10];
+      myIntArray[0] = 45;
+      myIntArray[1] = 1;
+      myIntArray[5] = 50;
+
+      double[] myDoubleArray = new double[10];
+      myDoubleArray[2] = 3.5;
+      System.out.println(myDoubleArray[2]);
+
+      // THE ARRAY INITIALIZER
+        int[] firstTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("first = " + firstTen[0]);
+        int arrayLength = firstTen.length;
+        System.out.println(arrayLength);
+        System.out.println("first = " + firstTen[arrayLength - 1]);
+
+        int[] newArray;
+//        newArray = new int[] {5, 4, 3, 2, 1};
+        newArray = new int[5];
+        for(int i = 0; i < newArray.length; i++) {
+            newArray[i] = newArray.length - i;
+        }
+        for(int i =0; i < newArray.length; i++){
+            System.out.print(newArray[i] + " ");
+        }
+
+        System.out.println();
+        for(int element : newArray) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(newArray));
+        Object objectVariables = newArray;
+        if(objectVariables instanceof  int[]){
+            System.out.println("ObjectVariable is really an in array?");
+        }
+
+        Object[] objectArray = new Object[3];
+        objectArray[0] = "Hello";
+        objectArray[1] = new StringBuilder("World");
+        objectArray[2] = newArray;
     }
 }
